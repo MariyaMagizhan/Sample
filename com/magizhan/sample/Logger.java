5af474ca4816dbde54e581b17f373bde1ca79c25
magil <mariyamahizhan@gmail.com>
2022-11-10 18:49:00 +0530

    Initial Commit message

---
+++ com/magizhan/sample/Logger.java
@@ -0,0 +1,7 @@
+package com.magizhan.sample;
+
+public class Logger {
+    public static void debug(String message) {
+        System.out.println(message);
+    }
+}
---
+++ com/magizhan/sample/MainApplication.java
@@ -0,0 +1,7 @@
+package com.magizhan.sample;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Logger.debug("Hello.!");
+    }
+}
