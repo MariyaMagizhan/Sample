96074cd40ae7b3f0776f555abcb4d97e4c36d709
magil <mariyamahizhan@gmail.com>
2022-11-10 18:52:15 +0530

    Debug logger added

--- com/magizhan/sample/MainApplication.java
+++ com/magizhan/sample/MainApplication.java
@@ -3,5 +3,6 @@ package com.magizhan.sample;
 public class MainApplication {
     public static void main(String[] args) {
         Logger.debug("Hello.!");
+        Logger.debug("World.!");
     }
 }
